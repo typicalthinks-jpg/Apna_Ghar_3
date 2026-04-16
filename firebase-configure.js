b109db363f9c3b5c5e640e663ef269bf91d613f5
typicalthinks-jpg <typicalthinks@gmail.com>
2026-04-17 01:50:01 +0530

    Apna Ghar complete project with all features

---
+++ firebase-configure.js
@@ -0,0 +1,20 @@
+
+//   Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+//   TODO: Add SDKs for Firebase products that you want to use
+//   https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig =
+   {
+    apiKey: "AIzaSyA_OIuWvXSkUn20h2EZEHYEleA3FHJNsvA",
+    authDomain: "apnaghar-3.firebaseapp.com",
+    projectId: "apnaghar-3",
+    storageBucket: "apnaghar-3.firebasestorage.app",
+    messagingSenderId: "612810268433",
+    appId: "1:612810268433:web:21dc5aed5869a1fea636b0"
+  }
+  ;
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
